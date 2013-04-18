50938104e0596f9be96eec7fed4a989b044b683d
Cel Tico <cel@cenics.net>
2013-04-18 20:49:28 +0200

    db

--- db/qumodules-app-demo.sql
+++ db/qumodules-app-demo.sql
@@ -4,6 +4,22 @@
 
 -- --------------------------------------------------------
 
+
+CREATE TABLE IF NOT EXISTS `qu-chat` (
+  `id_chat` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) DEFAULT NULL,
+  `id_user_parent` int(11) DEFAULT NULL,
+  `id_resource` int(11) DEFAULT NULL,
+  `id_resource_parent` int(11) DEFAULT NULL,
+  `type` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `name` text COLLATE utf8_unicode_ci,
+  `name_parent` text COLLATE utf8_unicode_ci,
+  `message` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id_chat`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+
 --
 -- Estructura de la taula `qu-other-demo`
 --
